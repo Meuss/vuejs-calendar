cdb735a43bf276115bbf9208fe0bd483ff67e576
Thomas Miller <tmi@macmac.ch>
2017-03-20 11:25:36 +0100

    finished lesson 131

---
+++ src/components/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div v-for="week in weeks">
+    week
+      <div v-for="day in week">{{ day }}</div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        month: 5,
+        year: 2017
+      };
+    },
+    computed: {
+      days() {
+
+        // Generating all days in current month
+        let days = [];
+        let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D');
+        do {
+          days.push(currentDay);
+          currentDay = this.$moment(currentDay).add(1, 'days');
+        } while ((currentDay.month() + 1) === this.month);
+
+        // Add previous days to start
+        currentDay = this.$moment(days[0]);
+
+        const SUNDAY = 0;
+        const MONDAY = 1;
+
+        if(currentDay.day() !== MONDAY) {
+          do {
+            currentDay = this.$moment(currentDay).subtract(1, 'days');
+            days.unshift(currentDay);
+          } while (currentDay.day() !== MONDAY);          
+        }
+
+        // Add following days to end
+        currentDay = this.$moment(days[days.length - 1]);
+
+        if(currentDay.day() !== SUNDAY) {
+          do {
+            currentDay = this.$moment(currentDay).add(1, 'days');
+            days.push(currentDay);
+          } while (currentDay.day() !== SUNDAY);
+        }
+
+        return days;
+      },
+      weeks() {
+        let weeks = [];
+        let week = [];
+
+        for (let day of this.days) {
+          week.push(day);
+          if (week.length === 7) {
+            weeks.push(week);
+            week = [];
+          }
+        }
+        return weeks;
+      }
+    }
+  }
+</script>
--- src/web.entry.js
+++ src/web.entry.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import './style.scss';
+
+import moment from 'moment-timezone';
+moment.tz.setDefault('UTC');
+Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment} });
+
+import App from './components/App.vue';
 
 new Vue({
   el: '#app',
   data: {
-    msg: 'Hello World'
+    moment
+  },
+  components: {
+    App
   }
 });
